06195779d016862f5534657e6e714aef64b095f1
James Bridges <masterbridges@gmail.com>
2019-12-18 21:58:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr)
+<<<<<<< HEAD
   arr.map do |name|
     badge_maker(name)
   end
@@ -22,4 +23,30 @@ def printer(arr)
   assign_rooms(arr).each do |assignment|
     puts assignment
   end
+=======
+  arr.each_with_index do |name, i|
+    arr[i] = "Hello, my name is #{name}."
+  end
+  return arr
+end
+
+def assign_rooms(arr)
+  arr.each_with_index do |name, i|
+    arr[i] = "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  end
+  return arr
+end
+
+def printer(arr)
+ 
+  
+  batch_badge_creator(arr).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(arr).each do |assignment|
+    puts assignment
+  end
+  
+>>>>>>> d0a61889ff322d1fc65e830d6b5ac51e4a50e0e4
 end
